45119097512e599c7ccbbea037afb4c016349087
thadeu <tadeuu@gmail.com>
2025-01-16 02:34:18 +0000

    build dist v1.0.12

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,17 @@
 type Options = {
     strict?: boolean;
     keepTypeof?: boolean;
+    stringifyTypeof?: boolean;
+    [key: string]: any;
 };
 declare class DryReplacer {
     data: object;
     strict?: boolean;
     keepTypeof?: boolean;
+    stringifyTypeof?: boolean;
     constructor(data: object, options?: Options);
     replaceValue(key: string, value: any, data: object, template: object): void;
     recursiveReplace(data: object, template: any): object;
-    try(jsonToParse: string): object;
+    try(jsonToParse: string, options?: {}): object;
 }
 export default DryReplacer;
